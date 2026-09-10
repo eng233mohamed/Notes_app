bbf752443a747eb6dadc1b0483c67ea8466daa77
eng233mohamed <mohamed5rrrrrr@gmail.com>
2026-09-09 22:09:56 -0700

    create dark theme and homepage view

---
+++ lib/views/Home_Page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  const Home_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData(brightness: Brightness.dark);
+    return Scaffold();
+  }
+}
